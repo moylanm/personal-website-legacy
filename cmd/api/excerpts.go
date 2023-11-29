c5e59e832a0947c985e474fcf529688ecf024b35
Myles Moylan <moylanm@fastmail.com>
2023-11-29 15:19:30 -0800

    Add excerpt update handler

--- cmd/api/excerpts.go
+++ cmd/api/excerpts.go
@@ -69,3 +69,66 @@ func (app *application) showExcerptHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) updateExcerptHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	excerpt, err := app.models.Excerpts.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	var input struct {
+		Author *string `json:"author"`
+		Work   *string `json:"work"`
+		Text   *string `json:"text"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if input.Author != nil {
+		excerpt.Author = *input.Author
+	}
+	if input.Work != nil {
+		excerpt.Work = *input.Work
+	}
+	if input.Text != nil {
+		excerpt.Text = *input.Text
+	}
+
+	v := validator.New()
+
+	if data.ValidateExcerpt(v, excerpt); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Excerpts.Update(excerpt)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+	}
+
+	err = app.writeJSON(w, http.StatusOK, nil, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
