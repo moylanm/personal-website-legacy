33c9037cfaecad24e39821270de3293fda9b746c
Myles Moylan <moylanm@fastmail.com>
2023-11-30 14:42:55 -0800

    Fix typo

--- migrations/000002_add_excerpts_indexes.up.sql
+++ migrations/000002_add_excerpts_indexes.up.sql
@@ -1,2 +1,2 @@
 CREATE INDEX IF NOT EXISTS excerpts_author_idx ON excerpts USING GIN (to_tsvector('simple', author));
-CREATE INDEX IF NOT EXISTS excerpts_tags_idx ON movies USING GIN (tags);
+CREATE INDEX IF NOT EXISTS excerpts_tags_idx ON excerpts USING GIN (tags);
