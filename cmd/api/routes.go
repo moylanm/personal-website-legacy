14667c081805cfd712d29f9cdba40b00fcf02bae
Myles Moylan <moylanm@fastmail.com>
2023-11-30 13:54:59 -0800

    Update router to handle excerpt paths

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,7 +9,16 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.healthcheckHandler)
 
-	return router
+	router.HandlerFunc(http.MethodGet, "/excerpts", app.listExcerptsHandler)
+	router.HandlerFunc(http.MethodPost, "/excerpts", app.authenticate(app.createExcerptHandler))
+	router.HandlerFunc(http.MethodGet, "/excerpts/:id", app.showExcerptHandler)
+	router.HandlerFunc(http.MethodPatch, "/excerpts/:id", app.authenticate(app.updateExcerptHandler))
+	router.HandlerFunc(http.MethodDelete, "/excerpts/:id", app.authenticate(app.deleteExcerptHandler))
+
+	return app.recoverPanic(app.rateLimit(router))
 }
