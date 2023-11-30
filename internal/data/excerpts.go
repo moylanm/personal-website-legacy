8857f704f5ac855114dd7353047cd00ae79b9f51
Myles Moylan <moylanm@fastmail.com>
2023-11-30 14:35:23 -0800

    Add checks for tags

--- internal/data/excerpts.go
+++ internal/data/excerpts.go
@@ -31,6 +31,9 @@ func ValidateExcerpt(v *validator.Validator, excerpt *Excerpt) {
 
 	v.Check(excerpt.Body != "", "body", "must be provided")
 
+	v.Check(excerpt.Tags != nil, "tags", "must be provided")
+	v.Check(len(excerpt.Tags) >= 1, "tags", "must contain at least one tag")
+	v.Check(len(excerpt.Tags) <= 5, "tags", "must not contain more than 5 tags")
 	v.Check(validator.Unique(excerpt.Tags), "tags", "must not contain duplicate values")
 }
 
