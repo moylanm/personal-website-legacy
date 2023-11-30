c1a5af46c011f306f46fba75bf761843be32831d
Myles Moylan <moylanm@fastmail.com>
2023-11-29 16:09:01 -0800

    Add Delete method

--- internal/data/excerpts.go
+++ internal/data/excerpts.go
@@ -101,3 +101,32 @@ func (e ExcerptModel) Update(excerpt *Excerpt) error {
 
 	return nil
 }
+
+func (e ExcerptModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+		DELETE FROM excerpts
+		WHERE id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	result, err := e.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
+}
