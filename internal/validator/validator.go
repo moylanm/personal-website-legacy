d612341403a925d9bf5e6e700a499bd4fd54cf9d
Myles Moylan <moylanm@fastmail.com>
2023-11-30 11:09:11 -0800

    Add Unique helper

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -30,3 +30,12 @@ func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	return slices.Contains(permittedValues, value)
 }
 
+func Unique[T comparable](values []T) bool {
+	uniqueValues := make(map[T]bool)
+
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+
+	return len(values) == len(uniqueValues)
+}
