c856aba3d6904701ebef2b207ec735f057401a8f
Myles Moylan <moylanm@fastmail.com>
2023-12-09 07:35:56 -0800

    Add logic and structure for page and page size

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,15 @@
 package data
 
 import (
+	"math"
 	"strings"
 
 	"mylesmoylan.net/internal/validator"
 )
 
 type Filters struct {
+	Page		 int
+	PageSize	 int
 	Sort         string
 	SortSafeList []string
 }
@@ -29,7 +32,19 @@ func (f Filters) sortDirection() string {
 	return "ASC"
 }
 
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
 func ValidateFilters(v *validator.Validator, f Filters) {
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
 }
 
@@ -44,7 +59,7 @@ type Metadata struct {
 	TotalRecords   int      `json:"total_records,omitempty"`
 }
 
-func calculateMetadata(totalRecords int, selectedAuthor string, authors []string, filters Filters) Metadata {
+func calculateMetadata(totalRecords, page, pageSize int, selectedAuthor string, authors []string, filters Filters) Metadata {
 	if totalRecords == 0 {
 		return Metadata{}
 	}
@@ -53,6 +68,10 @@ func calculateMetadata(totalRecords int, selectedAuthor string, authors []string
 		Sort:           filters.Sort,
 		SelectedAuthor: selectedAuthor,
 		Authors:        authors,
+		CurrentPage:    page,
+		PageSize:       pageSize,
+		FirstPage:      1,
+		LastPage:       int(math.Ceil(float64(totalRecords) / float64(pageSize))),
 		TotalRecords:   totalRecords,
 	}
 }
