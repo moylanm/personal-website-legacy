81a04f6f6e171d09d6137ed645b0068589ad69c2
Myles Moylan <moylanm@fastmail.com>
2024-01-02 07:45:06 -0800

    Remove health check and make small hygiene changes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,17 +14,16 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowedHandler = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	fileServer := http.FileServer(http.FS(ui.Files))
-
 	router.PathPrefix("/static/").Handler(fileServer)
 
-	router.HandleFunc("/healthcheck", app.healthcheckHandler).Methods(http.MethodGet)
-
-	router.HandleFunc("/excerpts/{id:[0-9]+}", app.authenticate(app.deleteExcerpt)).Methods(http.MethodDelete)
-	router.HandleFunc("/excerpts/{id:[0-9]+}", app.authenticate(app.updateExcerpt)).Methods(http.MethodPatch)
-	router.HandleFunc("/excerpts/{id:[0-9]+}", app.showExcerpt).Methods(http.MethodGet)
+	excerptsPath := "/excerpts/{id:[0-9]+}"
+	router.HandleFunc(excerptsPath, app.authenticate(app.deleteExcerpt)).Methods(http.MethodDelete)
+	router.HandleFunc(excerptsPath, app.authenticate(app.updateExcerpt)).Methods(http.MethodPatch)
+	router.HandleFunc(excerptsPath, app.showExcerpt).Methods(http.MethodGet)
 	router.HandleFunc("/excerpts/json", app.authenticate(app.listExcerptsJson)).Methods(http.MethodGet)
 	router.HandleFunc("/excerpts", app.authenticate(app.createExcerpt)).Methods(http.MethodPost)
 	router.HandleFunc("/excerpts", app.listExcerpts).Methods(http.MethodGet)
+
 	router.HandleFunc("/about", app.about).Methods(http.MethodGet)
 	router.HandleFunc("/", app.home).Methods(http.MethodGet)
 
