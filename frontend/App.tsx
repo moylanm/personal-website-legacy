8f6cfc81f2dbc294247ff77db202b4b4cbe5b94e
Myles Moylan <moylanm@fastmail.com>
2024-01-28 12:55:45 -0800

    Move fetch logic outside of App component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,57 +1,15 @@
-import React, {useCallback, useEffect, useMemo, useReducer } from 'react';
-import axios from 'axios';
+import React, {useCallback, useMemo, useReducer } from 'react';
 import List from './List';
 import FilterForm from './FilterForm';
+import fetchExcerpts from './fetch';
 import { reducer, initialState } from './reducer';
-import { ActionType, Excerpt } from './types';
+import { ActionType } from './types';
 
-const BASE_API_ENDPOINT = 'https://mylesmoylan.net/excerpts/json';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    const source = axios.CancelToken.source();
-
-    const fetchData = async () => {
-
-      dispatch({ type: ActionType.ExcerptFetchInit });
-
-      try {
-        const response = await axios.get(`${BASE_API_ENDPOINT}`, {
-          cancelToken: source.token
-        });
-
-        const excerpts = response.data['excerpts'].map((excerpt: Excerpt): Excerpt => ({
-          id: excerpt.id,
-          author: excerpt.author,
-          work: excerpt.work,
-          body: excerpt.body
-        }));
-
-        const uniqueAuthors = [...new Set<string>(excerpts.map((excerpt: Excerpt) => excerpt.author))];
-
-        dispatch({
-          type: ActionType.ExcerptFetchSuccess,
-          payload: { excerpts, uniqueAuthors }
-        });
-      } catch (error) {
-        if (axios.isCancel(error)) {
-          console.log('Request canceled:', error.message);
-        } else {
-          console.error('Error fetching data:', error);
-        }
-
-        dispatch({ type: ActionType.ExcerptFetchFailure });
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      source.cancel('Component unmounted, request canceled');
-    };
-  }, [])
+  fetchExcerpts(dispatch);
 
   const handleSortChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch({
---
+++ frontend/fetch.ts
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { Action, ActionType, Excerpt } from "./types";
+
+const BASE_API_ENDPOINT = 'https://mylesmoylan.net/excerpts/json';
+
+const fetchExcerpts = (
+  dispatch: React.Dispatch<Action>
+) => {
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    const fetchData = async () => {
+      dispatch({ type: ActionType.ExcerptFetchInit });
+
+      try {
+        const response = await axios.get(`${BASE_API_ENDPOINT}`, {
+          cancelToken: source.token
+        });
+
+        const excerpts = response.data['excerpts'].map((excerpt: Excerpt): Excerpt => ({
+          id: excerpt.id,
+          author: excerpt.author,
+          work: excerpt.work,
+          body: excerpt.body
+        }));
+
+        const uniqueAuthors = [...new Set<string>(excerpts.map((excerpt: Excerpt) => excerpt.author))];
+
+        dispatch({
+          type: ActionType.ExcerptFetchSuccess,
+          payload: { excerpts, uniqueAuthors }
+        });
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log('Request canceled:', error.message);
+        } else {
+          console.error('Error fetching data:', error);
+        }
+
+        dispatch({ type: ActionType.ExcerptFetchFailure });
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      source.cancel('Component unmounted, request canceled');
+    };
+  }, [])
+};
+
+export default fetchExcerpts;
