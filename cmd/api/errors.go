f51fc9a2969043268f4b5963fd45ae27212cb2cf
Myles Moylan <moylanm@fastmail.com>
2024-01-29 11:28:19 -0800

    Consolidate status and message to HTTPError struct

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,17 +6,28 @@ import (
 	"strings"
 )
 
+type HTTPError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e HTTPError) Error() string {
+	return e.Message
+}
+
 func (app *application) logError(r *http.Request, err error) {
 	var (
 		method = r.Method
 		uri    = r.URL.RequestURI()
+		ip     = r.RemoteAddr
 	)
 
-	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	app.logger.Error(err.Error(), "method", method, "uri", uri, "ip", ip)
 }
 
-func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
-	env := envelope{"error": message}
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, httpErr HTTPError) {
+	status := httpErr.StatusCode
+	message := httpErr.Message
 
 	if strings.HasPrefix(r.Header.Get("Accept"), "text/html") {
 		data := app.newTemplateData()
@@ -24,6 +35,7 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 		data.ErrorMessage = message
 		app.render(w, r, status, "error.tmpl", data)
 	} else {
+		env := envelope{"error": message}
 		err := app.writeJSON(w, status, env)
 		if err != nil {
 			app.logError(r, err)
@@ -35,39 +47,73 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 
-	message := "the server encountered a problem and could not process your request"
-	app.errorResponse(w, r, http.StatusInternalServerError, message)
+	httpErr := HTTPError{
+		StatusCode: http.StatusInternalServerError,
+		Message:    "the server encountered a problem and could not process your request",
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
-	message := "the requested resource could not be found"
-	app.errorResponse(w, r, http.StatusNotFound, message)
+	httpErr := HTTPError{
+		StatusCode: http.StatusNotFound,
+		Message:    "the requested resource could not be found",
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
 
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
-	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+	httpErr := HTTPError{
+		StatusCode: http.StatusMethodNotAllowed,
+		Message:    fmt.Sprintf("the %s method is not supported for this resource", r.Method),
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+	httpErr := HTTPError{
+		StatusCode: http.StatusBadRequest,
+		Message:    err.Error(),
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
 
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
-	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+	httpErr := HTTPError{
+		StatusCode: http.StatusUnprocessableEntity,
+		Message:    fmt.Sprintf("%v", errors),
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
 
 func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
-	message := "unable to update the record due to an edit conflict, please try again"
-	app.errorResponse(w, r, http.StatusConflict, message)
+	httpErr := HTTPError{
+		StatusCode: http.StatusConflict,
+		Message:    "unable to update the record due to an edit conflict, please try again",
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
 
 func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
-	message := "rate limit exceeded"
-	app.errorResponse(w, r, http.StatusTooManyRequests, message)
+	httpErr := HTTPError{
+		StatusCode: http.StatusTooManyRequests,
+		Message:    "rate limit exceeded",
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
 
 func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
-	message := "invalid authentication credentials"
-	app.errorResponse(w, r, http.StatusUnauthorized, message)
+	httpErr := HTTPError{
+		StatusCode: http.StatusUnauthorized,
+		Message:    "invalid authentication credentials",
+	}
+
+	app.errorResponse(w, r, httpErr)
 }
