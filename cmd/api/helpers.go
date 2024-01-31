3a78c7f0ebec45b879aa744d7cf95945241b5140
Myles Moylan <moylanm@fastmail.com>
2024-01-30 16:30:22 -0800

    Refactor to improve structure and readability of readJSON

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -55,44 +55,8 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 	dec := json.NewDecoder(r.Body)
 	dec.DisallowUnknownFields()
 
-	err := dec.Decode(dst)
-	if err != nil {
-		var (
-			syntaxError           *json.SyntaxError
-			unmarshalTypeError    *json.UnmarshalTypeError
-			invalidUnmarshalError *json.InvalidUnmarshalError
-			maxBytesError         *http.MaxBytesError
-		)
-
-		switch {
-		case errors.As(err, &syntaxError):
-			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			return errors.New("body contains badly-formed JSON")
-
-		case errors.As(err, &unmarshalTypeError):
-			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
-			}
-			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
-
-		case errors.Is(err, io.EOF):
-			return errors.New("body must not be empty")
-
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			return fmt.Errorf("body contains unknown key %s", fieldName)
-
-		case errors.As(err, &maxBytesError):
-			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
-
-		case errors.As(err, &invalidUnmarshalError):
-			panic(err)
-
-		default:
-			return err
-		}
+	if err := dec.Decode(dst); err != nil {
+		return handleJSONDecodeError(err)
 	}
 
 	// Check to ensure there is no more data after the JSON object
@@ -101,6 +65,45 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 	}
 
 	return nil
+}
+
+func handleJSONDecodeError(err error) error {
+	var (
+		syntaxError           *json.SyntaxError
+		unmarshalTypeError    *json.UnmarshalTypeError
+		invalidUnmarshalError *json.InvalidUnmarshalError
+		maxBytesError         *http.MaxBytesError
+	)
+
+	switch {
+	case errors.As(err, &syntaxError):
+		return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+	case errors.Is(err, io.ErrUnexpectedEOF):
+		return errors.New("body contains badly-formed JSON")
+
+	case errors.As(err, &unmarshalTypeError):
+		if unmarshalTypeError.Field != "" {
+			return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+		}
+		return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+	case errors.Is(err, io.EOF):
+		return errors.New("body must not be empty")
+
+	case strings.HasPrefix(err.Error(), "json: unknown field "):
+		fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+		return fmt.Errorf("body contains unknown key %s", fieldName)
+
+	case errors.As(err, &maxBytesError):
+		return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+
+	case errors.As(err, &invalidUnmarshalError):
+		panic(err)
+
+	default:
+		return err
+	}
 }
 
 func (app *application) readString(qs url.Values, key string, defaultValue string) string {
