b0dcf4bace12f70b0a2bc1761c91acc5e319736c
Myles Moylan <moylanm@fastmail.com>
2024-02-10 12:56:01 -0800

    Create users table

---
+++ migrations/000006_create_users_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+	id serial PRIMARY KEY,
+	name varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	hashed_password char(60) NOT NULL,
+	created timestamp(0) without time zone NOT NULL
+);
+
+ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
