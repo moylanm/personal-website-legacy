824134cadffe20763b0028f09d54dab79a131d36
Myles Moylan <moylanm@fastmail.com>
2024-02-10 10:58:18 -0800

    Add middleware for adding HTTP security headers

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -14,6 +14,20 @@ import (
 	"golang.org/x/time/rate"
 )
 
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Security-Policy",
+			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
