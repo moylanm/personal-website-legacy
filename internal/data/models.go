87200565826e516f5ccbe84d3a78be0c56244e5e
Myles Moylan <moylanm@fastmail.com>
2024-02-10 14:10:24 -0800

    Add UserModel to Models struct

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,10 +12,12 @@ var (
 
 type Models struct {
 	Excerpts ExcerptModel
+	Users    UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Excerpts: ExcerptModel{DB: db},
+		Users:    UserModel{DB: db},
 	}
 }
