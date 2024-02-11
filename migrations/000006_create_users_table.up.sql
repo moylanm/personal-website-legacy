ecee980b1240c8f61fa56dbed030527d17f88973
Myles Moylan <moylanm@fastmail.com>
2024-02-11 10:40:36 -0800

    Modify created field to default to NOW()

--- migrations/000006_create_users_table.up.sql
+++ migrations/000006_create_users_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
 	name varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
 	hashed_password char(60) NOT NULL,
-	created timestamp(0) without time zone NOT NULL
+	created timestamp(0) without time zone NOT NULL DEFAULT NOW()
 );
 
 ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
