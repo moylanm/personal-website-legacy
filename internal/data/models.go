e16a4c3ec6991a6f769b4d8d577171f4c494a4cd
Myles Moylan <moylanm@fastmail.com>
2024-02-11 18:34:13 -0800

    Formatting

--- internal/data/models.go
+++ internal/data/models.go
@@ -7,9 +7,9 @@ import (
 )
 
 var (
-	ErrRecordNotFound = errors.New("record not found")
-	ErrEditConflict   = errors.New("edit conflict")
-	ErrDuplicateEmail = errors.New("duplicate email")
+	ErrRecordNotFound     = errors.New("record not found")
+	ErrEditConflict       = errors.New("edit conflict")
+	ErrDuplicateEmail     = errors.New("duplicate email")
 	ErrInvalidCredentials = errors.New("invalid credentials")
 )
 
