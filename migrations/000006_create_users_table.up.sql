ebc893ef798efb6831163f27d7450f933d38a25f
Myles Moylan <moylanm@fastmail.com>
2024-02-11 16:06:36 -0800

    Refactor for better query

--- migrations/000006_create_users_table.up.sql
+++ migrations/000006_create_users_table.up.sql
@@ -1,9 +1,7 @@
-CREATE TABLE users (
-	id serial PRIMARY KEY,
-	name varchar(255) NOT NULL,
-	email varchar(255) NOT NULL,
-	hashed_password char(60) NOT NULL,
-	created timestamp(0) without time zone NOT NULL DEFAULT NOW()
+CREATE TABLE IF NOT EXISTS users (
+	id bigserial PRIMARY KEY,
+	name text NOT NULL,
+	email citext UNIQUE NOT NULL,
+	password_hash bytea NOT NULL,
+	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
-
-ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
