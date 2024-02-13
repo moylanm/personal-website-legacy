a23dda67ed2792432d3918e0fa587a4641f1cc5c
Myles Moylan <moylanm@fastmail.com>
2024-02-13 11:21:40 -0800

    Create logic for setting up metrics

---
+++ cmd/api/metrics.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	"expvar"
+	"runtime"
+)
+
+func publishMetrics(db *sql.DB) {
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	
+}
