e8bde40fbaa9ec60224e82c6c7cd5828b34e779c
Myles Moylan <moylanm@fastmail.com>
2024-02-12 17:47:30 -0800

    Add Strict-Transport-Security header

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -20,6 +20,7 @@ func secureHeaders(next http.Handler) http.Handler {
 		w.Header().Set("Content-Security-Policy",
 			"default-src 'self'; connect-src 'self' https://www.mylesmoylan.net https://mylesmoylan.net; object-src 'none'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 
+		w.Header().Set("Strict-Transport-Security", "31536000")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
