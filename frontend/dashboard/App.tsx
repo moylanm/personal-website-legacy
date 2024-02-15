8d7ca95b625eb25afc7be6d5a362e09b341b5bb3
Myles Moylan <moylanm@fastmail.com>
2024-02-15 14:17:02 -0800

    Begin to work out App component structure

--- frontend/dashboard/App.tsx
+++ frontend/dashboard/App.tsx
@@ -1,3 +1,22 @@
-const App = () => {
+import React, { useReducer } from 'react';
+import List from './List';
+import useFetchRequests from './useFetchRequests';
+import { reducer, initialState } from './reducer';
 
-}
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
+  useFetchRequests(dispatch);
+
+  if (state.isError) {
+    return <div className='error-message'>{state.errorMessage}</div>;
+  }
+
+  if (state.isLoading) {
+    return <div className='loading-message'>Loading requests...</div>;
+  }
+
+  return <List requests={state.requests} />
+};
+
+export default App;
