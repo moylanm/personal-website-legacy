f472558e4616dca6c24d4ab50c47e2542563330d
Myles Moylan <moylanm@fastmail.com>
2024-02-16 17:53:27 -0800

    Tie in new reducer capabilities

--- frontend/dashboard/App.tsx
+++ frontend/dashboard/App.tsx
@@ -1,12 +1,45 @@
-import React, { useReducer } from 'react';
+import React, { useCallback, useMemo, useReducer } from 'react';
+import FilterForm from './FilterForm';
 import RequestTable from './RequestTable';
 import useFetchRequests from './useFetchRequests';
 import { reducer, initialState } from './reducer';
+import { ActionType } from './types';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
   
   useFetchRequests(dispatch);
+
+  const handleSortChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: ActionType.SetSortOrder,
+      payload: event.target.value === 'oldest'
+    });
+  }, []);
+
+  const handleIPAddressChange = useCallback((event: React.FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    let selectedIPAddresses: string[];
+
+    if (target.checked) {
+      selectedIPAddresses = [...state.selectedIPAddresses, target.value];
+    } else {
+      selectedIPAddresses = state.selectedIPAddresses.filter(ipAddress => ipAddress !== target.value);
+    }
+
+    dispatch({
+      type: ActionType.SetSelectedIPAddresses,
+      payload: selectedIPAddresses
+    });
+  }, [state.selectedIPAddresses]);
+
+  const sortedAndFilteredRequests = useMemo(() => {
+    const filteredRequests = state.requests.filter(request => {
+      return state.selectedIPAddresses.includes(request.ipAddress);
+    });
+
+    return state.reverseSort ? filteredRequests.reverse() : filteredRequests;
+  }, [state.requests, state.selectedIPAddresses, state.reverseSort]);
 
   if (state.isError) {
     return <div className='error-message'>{state.errorMessage}</div>;
@@ -16,7 +49,17 @@ const App = () => {
     return <div className='loading-message'>Loading requests...</div>;
   }
 
-  return <RequestTable requests={state.requests} />
+  return (
+    <>
+      <FilterForm
+        selectedSortOrder={state.reverseSort}
+        ipAddresses={state.ipAddresses}
+        onSortChange={handleSortChange}
+        onIPAddrChange={handleIPAddressChange}
+      />
+      <RequestTable requests={sortedAndFilteredRequests} />
+    </>
+  );
 };
 
 export default App;
