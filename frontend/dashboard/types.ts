1b714f22154817929b603276af434a118dcdb4e1
Myles Moylan <moylanm@fastmail.com>
2024-02-17 12:06:48 -0800

    Add structure for refetch

--- frontend/dashboard/types.ts
+++ frontend/dashboard/types.ts
@@ -18,13 +18,16 @@ export type AppState = {
 	reverseSort: boolean;
 	isLoading: boolean;
 	errorMessage: string;
-	isError: boolean;
+	isInitError: boolean;
+	isRefetchError: boolean;
 }
 
 export enum ActionType {
 	InitialFetchInit = 'INITIAL_FETCH_INIT',
 	InitialFetchSuccess = 'INITIAL_FETCH_SUCCESS',
 	InitialFetchFailure = 'INITIAL_FETCH_FAILURE',
+	RefetchSuccess = 'REFETCH_SUCCESS',
+	RefetchFailure = 'REFETCH_FAILURE',
 	SetSortOrder = 'SET_SORT_ORDER',
 	SetSelectedIPAddresses = 'SET_SELECTED_IP_ADDRESSES',
 	Reset = 'RESET'
@@ -36,10 +39,17 @@ type InitialFetchSuccessPayload = {
 	selectedIPAddresses: string[];
 };
 
+type RefetchSuccessPayload = {
+	requests: Request[];
+	ipAddresses: string[];
+};
+
 export type Action =
 	| { type: ActionType.InitialFetchInit }
 	| { type: ActionType.InitialFetchSuccess; payload: InitialFetchSuccessPayload }
 	| { type: ActionType.InitialFetchFailure; payload: string }
+	| { type: ActionType.RefetchSuccess; payload: RefetchSuccessPayload }
+	| { type: ActionType.RefetchFailure; payload: string }
 	| { type: ActionType.SetSelectedIPAddresses; payload: string[] }
 	| { type: ActionType.SetSortOrder; payload: boolean }
 	| { type: ActionType.Reset }
