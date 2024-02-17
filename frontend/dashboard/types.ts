f41fa0853026a110a76cca4fb21098042195d724
Myles Moylan <moylanm@fastmail.com>
2024-02-16 17:51:37 -0800

    Reverse sort, ip addresses, and selected ip addresses

--- frontend/dashboard/types.ts
+++ frontend/dashboard/types.ts
@@ -13,6 +13,9 @@ export type Request = {
 
 export type AppState = {
 	requests: Request[];
+	ipAddresses: string[];
+	selectedIPAddresses: string[];
+	reverseSort: boolean;
 	isLoading: boolean;
 	errorMessage: string;
 	isError: boolean;
@@ -22,12 +25,24 @@ export enum ActionType {
 	RequestsFetchInit = 'REQUESTS_FETCH_INIT',
 	RequestsFetchSuccess = 'REQUESTS_FETCH_SUCCESS',
 	RequestsFetchFailure = 'REQUESTS_FETCH_FAILURE',
+	SetSortOrder = 'SET_SORT_ORDER',
+	SetSelectedIPAddresses = 'SET_SELECTED_IP_ADDRESSES',
+	Reset = 'RESET'
 }
+
+type RequestsFetchSuccessPayload = {
+	requests: Request[];
+	ipAddresses: string[];
+	selectedIPAddresses: string[];
+};
 
 export type Action =
 	| { type: ActionType.RequestsFetchInit }
-	| { type: ActionType.RequestsFetchSuccess; payload: Request[] }
-	| { type: ActionType.RequestsFetchFailure; payload: string };
+	| { type: ActionType.RequestsFetchSuccess; payload: RequestsFetchSuccessPayload }
+	| { type: ActionType.RequestsFetchFailure; payload: string }
+	| { type: ActionType.SetSelectedIPAddresses; payload: string[] }
+	| { type: ActionType.SetSortOrder; payload: boolean }
+	| { type: ActionType.Reset }
 
 export type ApiResponse = {
 	requests: Request[];
