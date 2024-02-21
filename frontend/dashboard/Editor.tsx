658d188de357cf64318ba2191c9b74ef90c1eaa7
Myles Moylan <moylanm@fastmail.com>
2024-02-21 11:40:46 -0800

    Stub out Editor component

---
+++ frontend/dashboard/Editor.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Action, AppState } from './types';
+
+const Editor = (
+  state: AppState,
+  dispatch: React.Dispatch<Action>
+) => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Editor;
