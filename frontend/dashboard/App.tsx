76b39ef8290b45fa13a38ee9eee7c53e09fbfed7
Myles Moylan <moylanm@fastmail.com>
2024-02-22 11:24:58 -0800

    Use Material tabs

--- frontend/dashboard/App.tsx
+++ frontend/dashboard/App.tsx
@@ -1,46 +1,50 @@
 import React, { useReducer, useState } from 'react';
 import { initialState, reducer } from './reducer';
-import Logs from './logs/Logs'
+import useInitialFetch from './useInitialFetch';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import Publisher from './Publisher';
 import Editor from './Editor';
-import useInitialFetch from './useInitialFetch';
+import Logs from './logs/Logs'
+
+const tabStyle = {
+  opacity: 1,
+  '&:hover': {
+    backgroundColor: 'transparent',
+    color: '#4EB722'
+  }
+}
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [activeTab, setActiveTab] = useState<string>('publish');
+  const [activeTab, setActiveTab] = useState<number>(0);
 
   useInitialFetch(dispatch);
 
-  const selectTab = (tabId: string) => {
+  const selectTab = (_: React.SyntheticEvent, tabId: number) => {
     setActiveTab(tabId);
   }
 
   return (
-    <>
-      <div className='tab-headers'>
-        <button onClick={() => selectTab('publish')} className={activeTab === 'publish' ? 'active' : ''}>
-          Publish
-        </button>
-        <button onClick={() => selectTab('edit')} className={activeTab === 'edit' ? 'active' : ''}>
-          Edit
-        </button>
-        <button onClick={() => selectTab('logs')} className={activeTab === 'logs' ? 'active' : ''}>
-          Logs
-        </button>
-        <button onClick={() => selectTab('metrics')} className={activeTab === 'metrics' ? 'active' : ''}>
-          Metrics
-        </button>
-      </div>
+    <Box sx={{ backgroundColor: 'inherit' }}>
+      <Tabs
+        value={activeTab}
+        onChange={selectTab}
+        variant='fullWidth'
+        textColor='inherit'
+      >
+        <Tab label='Publish' value={0} sx={tabStyle} />
+        <Tab label='Edit' value={1} sx={tabStyle} />
+        <Tab label='Logs' value={2} sx={tabStyle} />
+        <Tab label='Metrics' value={3} sx={tabStyle}/>
+      </Tabs>
 
-      <hr />
-
-      <div className='tab-content'>
-        {activeTab === 'publish' && <Publisher state={state} dispatch={dispatch} />}
-        {activeTab === 'edit' && <Editor state={state} dispatch={dispatch} />}
-        {activeTab === 'logs' && <Logs />}
-        {activeTab === 'metrics' && <div>Metrics content...</div>}
-      </div>
-    </>
+      {activeTab === 0 && <Publisher state={state} dispatch={dispatch} />}
+      {activeTab === 1 && <Editor state={state} dispatch={dispatch} />}
+      {activeTab === 2 && <Logs />}
+      {activeTab === 3 && <div>Metrics content...</div>}
+    </Box>
   );
 };
 
