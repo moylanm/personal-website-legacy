fc12ce6451fdf5e9246b7e088f983c6aeafce165
Myles Moylan <moylanm@fastmail.com>
2024-02-22 11:24:35 -0800

    Add author, work, and body fields

--- frontend/dashboard/Publisher.tsx
+++ frontend/dashboard/Publisher.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Action, AppState } from './types';
+import TextField from '@mui/material/TextField';
 
 type PublisherProps = {
   state: AppState;
@@ -10,8 +11,20 @@ const Publisher: React.FC<PublisherProps> = ({
   state,
   dispatch
 }) => {
+
+  if (state.isError) {
+    return <div className='error-message'>{state.errorMessage}</div>;
+  }
+
+  if (state.isLoading) {
+    return <div className='message'>Loading...</div>
+  }
+
   return (
     <>
+      <TextField fullWidth label='Author' variant='outlined' margin='normal' />
+      <TextField fullWidth label='Work' variant='outlined' margin='normal' />
+      <TextField fullWidth label='Body' variant='outlined' margin='normal' multiline rows={10} />
     </>
   );
 };
