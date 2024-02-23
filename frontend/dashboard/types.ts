29fa2f50aa64e09ac458f0911b0ee5ad70bc0b46
Myles Moylan <moylanm@fastmail.com>
2024-02-23 11:01:06 -0800

    Add structure for publish form state

--- frontend/dashboard/types.ts
+++ frontend/dashboard/types.ts
@@ -9,6 +9,9 @@ export type AppState = {
 	excerpts: Excerpt[];
 	authors: string[];
 	works: { [author: string]: string[] };
+	authorField: string;
+	workField: string;
+	bodyField: string;
 	isLoading: boolean;
 	errorMessage: string;
 	isError: boolean;
@@ -18,6 +21,9 @@ export enum ActionType {
   InitialFetchInit = 'INITIAL_FETCH_INIT',
   InitialFetchSuccess = 'INITIAL_FETCH_SUCCESS',
   InitialFetchFailure = 'INITIAL_FETCH_FAILURE',
+	SetAuthorField = 'SET_AUTHOR_FIELD',
+	SetWorkField = 'SET_WORK_FIELD',
+	SetBodyField = 'SET_BODY_FIELD',
 };
 
 type InitialFetchSuccessPayload = {
@@ -29,7 +35,10 @@ type InitialFetchSuccessPayload = {
 export type Action =
 	| { type: ActionType.InitialFetchInit }
 	| { type: ActionType.InitialFetchSuccess; payload: InitialFetchSuccessPayload }
-	| { type: ActionType.InitialFetchFailure; payload: string };
+	| { type: ActionType.InitialFetchFailure; payload: string }
+	| { type: ActionType.SetAuthorField; payload: string }
+	| { type: ActionType.SetWorkField; payload: string }
+	| { type: ActionType.SetBodyField; payload: string };
 
 export type ApiResponse = {
 	excerpts: Excerpt[];
