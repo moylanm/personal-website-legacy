d9280b8471929b137f95c08de767211caf0f9439
Myles Moylan <moylanm@fastmail.com>
2024-02-22 18:11:39 -0800

    Integrate autocomplete with author and work fields

--- frontend/dashboard/Publisher.tsx
+++ frontend/dashboard/Publisher.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Action, AppState } from './types';
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+type WorkOption = {
+  author: string;
+  work: string;
+};
 
 type PublisherProps = {
   state: AppState;
@@ -11,6 +17,13 @@ const Publisher: React.FC<PublisherProps> = ({
   state,
   dispatch
 }) => {
+  const worksOptions = state.authors.reduce<WorkOption[]>((acc, author) => {
+    state.works[author].forEach(work => {
+      acc.push({ author: author, work: work})
+    });
+
+    return acc;
+  }, []);
 
   if (state.isError) {
     return <div className='error-message'>{state.errorMessage}</div>;
@@ -22,8 +35,18 @@ const Publisher: React.FC<PublisherProps> = ({
 
   return (
     <>
-      <TextField fullWidth label='Author' variant='outlined' margin='normal' />
-      <TextField fullWidth label='Work' variant='outlined' margin='normal' />
+      <Autocomplete
+        freeSolo
+        options={state.authors}
+        renderInput={(authors) => <TextField {...authors} label='Author' margin='normal' />}
+      />
+      <Autocomplete
+        freeSolo
+        options={worksOptions.sort((a, b) => -b.author.localeCompare(a.author))}
+        groupBy={(option) => option.author}
+        getOptionLabel={(option) => option.work}
+        renderInput={(params) => <TextField {...params} label='Work' margin='normal' />}
+      />
       <TextField fullWidth label='Body' variant='outlined' margin='normal' multiline rows={10} />
     </>
   );
