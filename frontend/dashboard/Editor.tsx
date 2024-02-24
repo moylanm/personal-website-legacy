4c36896793d9354b5fa03d9cb2a2bf9de8e8acc4
Myles Moylan <moylanm@fastmail.com>
2024-02-24 10:48:00 -0800

    Stub out logic for displaying excerpts

--- frontend/dashboard/Editor.tsx
+++ frontend/dashboard/Editor.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
-import { Action, AppState } from './types';
+import { Action, AppState, Excerpt } from './types';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+type ExcerptItemProps = {
+  excerpt: Excerpt;
+};
+
+const ExcerptItem: React.FC<ExcerptItemProps> = ({
+  excerpt
+}) => {
+  return (
+    <Accordion>
+      <AccordionSummary>
+        <Typography sx={{
+          fontStyle: 'Roboto, Helvetica, Arial, sans-serif',
+          padding: 0
+        }}>
+          {`${excerpt.id}: ${excerpt.author} - ${excerpt.work}`}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <TextField fullWidth label='Author' margin='normal' value={excerpt.author} />
+        <TextField fullWidth label='Work' margin='normal' value={excerpt.work} />
+        <TextField fullWidth multiline rows={10} label='Body' value={excerpt.body} />
+      </AccordionDetails>
+      <AccordionActions>
+        <Button>Delete</Button>
+        <Button>Update</Button>
+      </AccordionActions>
+    </Accordion>
+  );
+};
 
 type EditorProps = {
   state: AppState;
@@ -12,6 +49,7 @@ const Editor: React.FC<EditorProps> = ({
 }) => {
   return (
     <>
+      {state.excerpts.map(excerpt => <ExcerptItem excerpt={excerpt} />)}
     </>
   );
 };
