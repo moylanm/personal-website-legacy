3b324fbcd5f30f692e775fe86ca8e260dc8456bb
Myles Moylan <moylanm@fastmail.com>
2024-02-26 11:48:23 -0800

    Return RequestTable component

--- frontend/dashboard/Logs.tsx
+++ frontend/dashboard/Logs.tsx
@@ -1,16 +1,21 @@
 import React, { useCallback, useMemo } from 'react';
 import { Action, AppState } from './types';
+import RequestTable from './RequestTable';
 
 type LogsProps = {
   state: AppState;
   dispatch: React.Dispatch<Action>;
 }
 
-const Logs = ({
+const Logs: React.FC<LogsProps> = ({
   state,
   dispatch
 }) => {
-
+  return (
+    <>
+      <RequestTable requests={state.requests} />
+    </>
+  );
 };
 
 export default Logs;
