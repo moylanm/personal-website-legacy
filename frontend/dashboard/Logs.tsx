bd94d73f6bae3453cff0f1694d8beb3d7f03ddc0
Myles Moylan <moylanm@fastmail.com>
2024-02-26 15:49:52 -0800

    Add handler functions and memo

--- frontend/dashboard/Logs.tsx
+++ frontend/dashboard/Logs.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useMemo } from 'react';
-import { Action, AppState } from './types';
+import { Action, ActionType, AppState } from './types';
 import RequestTable from './RequestTable';
+import FilterForm from './FilterForm';
+import { clearLogs, fetchLogs } from './api';
 
 type LogsProps = {
   state: AppState;
@@ -11,9 +13,50 @@ const Logs: React.FC<LogsProps> = ({
   state,
   dispatch
 }) => {
+
+  const handlFetchDataClick = () => {
+    fetchLogs(dispatch, state.ipAddresses, state.renderKey);
+  };
+
+  const handleClearDataClick = () => {
+    clearLogs(dispatch, state.renderKey);
+  };
+
+  const handleIPAddressChange = (value: string, selected: boolean) => {
+    const ipAddresses = state.ipAddresses.map((ip) => {
+      return ip.value === value ? {value: ip.value, selected: !selected} : ip;
+    });
+
+
+    dispatch({
+      type: ActionType.SetIPAddresses,
+      payload: ipAddresses
+    });
+  };
+
+  const filteredRequests = useMemo(() => {
+    const selectedIPs = state.ipAddresses
+      .filter((ip) => ip.selected)
+      .map((ip) => ip.value);
+
+    return state.requests.filter((request) =>
+      selectedIPs.includes(request.ipAddress)
+    );
+  }, [state.requests, state.ipAddresses]);
+
   return (
     <>
-      <RequestTable requests={state.requests} />
+      <FilterForm 
+        renderKey={state.renderKey}
+        ipAddresses={state.ipAddresses}
+        onIPAddrChange={handleIPAddressChange}
+        onFetchDataClick={handlFetchDataClick}
+        onClearDataClick={handleClearDataClick}
+      />
+      {filteredRequests.length > 0
+        ? <RequestTable key={state.renderKey} requests={filteredRequests} />
+        : <div className='message'>No logs to render...</div>
+      }
     </>
   );
 };
