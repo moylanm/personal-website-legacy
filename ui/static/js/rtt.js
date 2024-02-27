c6481e46de2957ee7583d01c58bf156696000691
Myles Moylan <moylanm@fastmail.com>
2024-02-27 07:18:36 -0800

    Adjust where return to top button appears

--- ui/static/js/rtt.js
+++ ui/static/js/rtt.js
@@ -5,7 +5,7 @@ window.onscroll = function() {
 function scrollFunction() {
 	let rttButton = document.querySelector(".rtt");
 
-	if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+	if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
 		rttButton.style.display = "block";
 	} else {
 		rttButton.style.display = "none";
