64b3e94c57549a7fbbed7ec4b97de21511aa5bde
Myles Moylan <moylanm@fastmail.com>
2024-02-28 11:14:40 -0800

    Optimizations

--- frontend/dashboard/Editor.tsx
+++ frontend/dashboard/Editor.tsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState, Suspense } from 'react';
 import { Action, AppState, Excerpt } from './types';
 import { deleteExcerpt, fetchExcerpts, updateExcerpt } from './api';
+import useIntersectionObserver from './useIntersectionObserver';
 import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 const FONT_STYLE = 'Roboto, Helvetica, Arial, sans-serif';
 const CHUNK_SIZE = 15;
+
+const DeleteDialog = React.lazy(() => import('./DeleteDialog'));
 
 type EditorProps = {
   state: AppState;
@@ -28,25 +27,17 @@ const Editor: React.FC<EditorProps> = ({
   const [displayCount, setDisplayCount] = useState(CHUNK_SIZE);
   const loadMoreRef = useRef(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
+  useIntersectionObserver(
+    loadMoreRef,
+    (entries) => {
       if (entries[0].isIntersecting) {
-        setDisplayCount(prevCount => Math.min(prevCount + CHUNK_SIZE, state.excerpts.length));
+        setDisplayCount(prevCount => Math.min(prevCount + CHUNK_SIZE, state.excerpts.length))
       }
-    }, {
+    },
+    {
       rootMargin: '500px'
-    });
-
-    if (loadMoreRef.current) {
-      observer.observe(loadMoreRef.current);
     }
-
-    return () => {
-      if (loadMoreRef.current) {
-        observer.unobserve(loadMoreRef.current);
-      }
-    };
-  }, [state.excerpts]);
+  );
 
   useEffect(() => {
     if (state.excerptActionSuccess) {
@@ -67,7 +58,7 @@ type ItemProps = {
   dispatch: React.Dispatch<Action>;
 };
 
-const Item: React.FC<ItemProps> = ({
+const Item: React.FC<ItemProps> = React.memo(({
   excerpt,
   dispatch
 }) => {
@@ -77,24 +68,24 @@ const Item: React.FC<ItemProps> = ({
   const workRef = useRef<HTMLInputElement>();
   const bodyRef = useRef<HTMLInputElement>();
 
-  const handleClickOpenDialog = () => {
+  const handleClickOpenDialog = useCallback(() => {
     setOpenDialog(true);
-  };
+  }, []);
 
-  const handleCloseDialog = () => {
+  const handleCloseDialog = useCallback(() => {
     setOpenDialog(false);
-  };
+  }, []);
 
-  const handleDelete = () => {
+  const handleDelete = useCallback(() => {
     handleCloseDialog();
 
     deleteExcerpt(
       dispatch,
       excerpt.id
     );
-  };
+  }, [dispatch, excerpt.id]);
 
-  const handleUpdate = () => {
+  const handleUpdate = useCallback(() => {
     updateExcerpt(
       dispatch,
       excerpt.id,
@@ -102,7 +93,7 @@ const Item: React.FC<ItemProps> = ({
       workRef.current!.value,
       bodyRef.current!.value
     );
-  };
+  }, [dispatch, excerpt.id]);
 
   return (
     <>
@@ -148,46 +139,11 @@ const Item: React.FC<ItemProps> = ({
           <Button onClick={handleUpdate}>Update</Button>
         </AccordionActions>
       </Accordion>
-      <DeleteDialog excerpt={excerpt} open={openDialog} handleClose={handleCloseDialog} handleConfirm={handleDelete} />
+      <Suspense fallback={<div className='message'>Loading...</div>}>
+        <DeleteDialog excerpt={excerpt} open={openDialog} handleClose={handleCloseDialog} handleConfirm={handleDelete} />
+      </Suspense>
     </>
   );
-};
-
-type DeleteDialogProps = {
-  excerpt: Excerpt;
-  open: boolean;
-  handleClose: () => void;
-  handleConfirm: () => void;
-};
-
-const DeleteDialog: React.FC<DeleteDialogProps> = ({
-  excerpt,
-  open,
-  handleClose,
-  handleConfirm
-}) => {
-  return (
-    <Dialog
-    onClose={handleClose}
-    open={open}
-    >
-    <DialogTitle sx={{ fontSize: '1rem' }}>
-      {`${excerpt.id}: ${excerpt.author} - ${excerpt.work}`}
-    </DialogTitle>
-    <DialogContent>
-      <Typography sx={{
-        fontStyle: FONT_STYLE,
-        padding: 0
-      }}>
-        Are you sure you want to delete this excerpt?
-      </Typography>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={handleClose}>Cancel</Button>
-      <Button onClick={handleConfirm}>Delete</Button>
-    </DialogActions>
-    </Dialog>
-  );
-};
+});
 
 export default Editor;
