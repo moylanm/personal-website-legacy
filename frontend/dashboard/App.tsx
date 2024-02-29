6744b0aab36a39267633fcd444bf6d7630c3d70e
Myles Moylan <moylanm@fastmail.com>
2024-02-28 17:45:29 -0800

    Revert from using memoized components

--- frontend/dashboard/App.tsx
+++ frontend/dashboard/App.tsx
@@ -2,16 +2,12 @@ import React, { useCallback, useReducer, useState } from 'react';
 import { ActionType } from './types';
 import { initialState, reducer } from './reducer';
 import { useInitialFetch } from './api';
+import { StyledTab, StyledTabsBox } from './styled';
 import { SuccessSnackbar, ErrorSnackbar } from './Snackbar';
 import Tabs from '@mui/material/Tabs';
 import Publisher from './Publisher';
 import Editor from './Editor';
 import Logs from './Logs'
-import { StyledTab, StyledTabsBox } from './styled';
-
-const MemoizedPublisher = React.memo(Publisher);
-const MemoizedEditor = React.memo(Editor);
-const MemoizedLogs = React.memo(Logs);
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -43,10 +39,11 @@ const App = () => {
 
       <hr />
 
-      {activeTab === 0 && <MemoizedPublisher state={state} dispatch={dispatch} />}
-      {activeTab === 1 && <MemoizedEditor key={`editor-${state.renderKey}`} state={state} dispatch={dispatch} />}
-      {activeTab === 2 && <MemoizedLogs key={`logs-${state.renderKey}`} state={state} dispatch={dispatch} />}
+      {activeTab === 0 && <Publisher state={state} dispatch={dispatch} />}
+      {activeTab === 1 && <Editor key={`editor-${state.renderKey}`} state={state} dispatch={dispatch} />}
+      {activeTab === 2 && <Logs key={`logs-${state.renderKey}`} state={state} dispatch={dispatch} />}
       {activeTab === 3 && <div>Metrics content...</div>}
+
       <SuccessSnackbar state={state} handleClose={handleSnackbarClose} />
       <ErrorSnackbar state={state} handleClose={handleSnackbarClose} />
     </StyledTabsBox>
