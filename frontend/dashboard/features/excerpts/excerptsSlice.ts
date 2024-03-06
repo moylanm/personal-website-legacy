5f4373aaf68be7ccddc5fd906d7cd5939aa285cb
Myles Moylan <moylanm@fastmail.com>
2024-03-06 13:03:37 -0800

    #4 Set up for normalizing state

--- frontend/dashboard/features/excerpts/excerptsSlice.ts
+++ frontend/dashboard/features/excerpts/excerptsSlice.ts
@@ -1,10 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { Excerpt } from './types';
+
+const excerptsAdapter = createEntityAdapter({
+	sortComparer: (a: Excerpt, b: Excerpt) => a.id - b.id
+});
+
+const initialState = excerptsAdapter.getInitialState({
+	status: 'idle',
+	error: null
+});
 
 export const excerptsSlice = createSlice({
 	name: 'excerpts',
-	initialState: {
-
-	},
+	initialState,
 	reducers: {
 
 	}
--- frontend/dashboard/features/logs/logsSlice.ts
+++ frontend/dashboard/features/logs/logsSlice.ts
@@ -1,10 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { Request } from './types';
+
+const logsAdapter = createEntityAdapter({
+	sortComparer: (a: Request, b: Request) => a.id - b.id
+});
+
+const initialState = logsAdapter.getInitialState({
+	status: 'idle',
+	error: null
+});
 
 export const logsSlice = createSlice({
 	name: 'logs',
-	initialState: {
-
-	},
+	initialState,
 	reducers: {
 
 	}
