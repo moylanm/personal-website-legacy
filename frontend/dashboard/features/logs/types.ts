d88049634a0d099f9a1356a948a51ceb7aa80fcd
Myles Moylan <moylanm@fastmail.com>
2024-03-06 13:02:57 -0800

    #1 Add types for excerpts and logs slices

---
+++ frontend/dashboard/features/logs/types.ts
@@ -0,0 +1,14 @@
+export type Request = {
+	id: number;
+	timestamp: string;
+	ipAddress: string;
+	method: string;
+	path: string;
+	referer: string;
+	uaName: string;
+	uaOS: string;
+	uaDeviceType: string;
+	uaDeviceName: string;
+};
+
+
