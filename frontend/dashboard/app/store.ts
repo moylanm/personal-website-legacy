ac758d87be04e59b75614ee35dbe12dc57b0f08a
Myles Moylan <moylanm@fastmail.com>
2024-03-07 11:49:42 -0800

    Include api slice in store

--- frontend/dashboard/app/store.ts
+++ frontend/dashboard/app/store.ts
@@ -1,14 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import excerptsReducer from '../features/excerpts/excerptsSlice';
 import logsReducer from '../features/logs/logsSlice';
+import { apiSlice } from '../features/api/apiSlice';
 
 const store = configureStore({
   reducer: {
     excerpts: excerptsReducer,
-    logs: logsReducer
-  }
+    logs: logsReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
+export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
