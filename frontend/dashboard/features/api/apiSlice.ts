ddcb61e706f1d654949b5c4966fbb87cc2b28729
Myles Moylan <moylanm@fastmail.com>
2024-03-07 13:11:12 -0800

    Add type assertions to main excerpts fetch query

--- frontend/dashboard/features/api/apiSlice.ts
+++ frontend/dashboard/features/api/apiSlice.ts
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Excerpt } from '../excerpts/types';
 
 export const apiSlice = createApi({
+	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({ baseUrl: 'https://mylesmoylan.net' }),
 	endpoints: builder => ({
-		getExcerpts: builder.query({
+		getExcerpts: builder.query<Excerpt[], void>({
 			query: () => '/excerpts/json'
 		})
 	})
