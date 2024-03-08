1e5dfaf8e884f1918a8d88fd6baf2bfa54253275
Myles Moylan <moylanm@fastmail.com>
2024-03-08 14:20:59 -0800

    Add cases for handling queries

--- frontend/dashboard/features/logs/logsSlice.ts
+++ frontend/dashboard/features/logs/logsSlice.ts
@@ -1,4 +1,5 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter, createSlice, SerializedError } from '@reduxjs/toolkit';
+import { api } from '../api/apiSlice';
 import { Request } from './types';
 
 const logsAdapter = createEntityAdapter({
@@ -7,14 +8,36 @@ const logsAdapter = createEntityAdapter({
 
 const initialState = logsAdapter.getInitialState({
 	status: 'idle',
-	error: null
+	error: null as (SerializedError | null)
 });
 
 export const logsSlice = createSlice({
 	name: 'logs',
 	initialState,
-	reducers: {
-
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addMatcher(api.endpoints.getLogs.matchPending, (state) => {
+				state.status = 'loading';
+			})
+			.addMatcher(api.endpoints.getLogs.matchRejected, (state, { error }) => {
+				state.status = 'failed';
+				state.error = error;
+			})
+			.addMatcher(api.endpoints.getLogs.matchFulfilled, (state, { payload }) => {
+				state.status = 'succeeded';
+				state.ids = payload.map(log => log.id);
+				state.entities = payload.reduce((acc, log) => acc[log.id] = log, {});
+			})
+			.addMatcher(api.endpoints.clearLogs.matchRejected, (state, { error }) => {
+				state.status = 'failed';
+				state.error = error;
+			})
+			.addMatcher(api.endpoints.clearLogs.matchFulfilled, (state) => {
+				state.status = 'succeeded';
+				state.ids = [];
+				state.entities = {};
+			})
 	}
 });
 
