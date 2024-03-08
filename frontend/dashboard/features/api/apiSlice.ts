f20d91fbd49e4bf33731807af4a1a8b45452bbab
Myles Moylan <moylanm@fastmail.com>
2024-03-08 11:01:31 -0800

    Add query for publishing excerpt

--- frontend/dashboard/features/api/apiSlice.ts
+++ frontend/dashboard/features/api/apiSlice.ts
@@ -13,6 +13,25 @@ export const apiSlice = createApi({
 				return rawResult.excerpts;
 			}
 		}),
+		publishExcerpt: builder.mutation<Excerpt, Excerpt>({
+			query: excerpt => {
+				const formData = new FormData();
+				formData.append('csrf_token', csrfToken());
+				formData.append('author', excerpt.author);
+				formData.append('work', excerpt.work);
+				formData.append('body', excerpt.body);
+
+				return {
+					url: '/excerpts',
+					method: 'POST',
+					headers: { 'Content-Type': 'multipart/form-data;' },
+					body: formData,
+				}
+			},
+			transformResponse: (rawResult: { excerpt: Excerpt }) => {
+				return rawResult.excerpt;
+			}
+		}),
 		getLogs: builder.query<Request[], void>({
 			query: () => '/dashboard/request-logs',
 			transformResponse: (rawResult: { requests: Request[] }) => {
