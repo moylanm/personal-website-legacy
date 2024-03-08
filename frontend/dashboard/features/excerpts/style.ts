ee261810b1122ff68124a95205e571634c339589
Myles Moylan <moylanm@fastmail.com>
2024-03-08 13:45:28 -0800

    Add style for TextField

--- frontend/dashboard/features/excerpts/style.ts
+++ frontend/dashboard/features/excerpts/style.ts
@@ -1,11 +1,16 @@
 import { styled } from '@mui/system';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import DialogTitle from '@mui/material/DialogTitle';
 
 export const StyledTypography = styled(Typography)({
   fontStyle: 'Roboto, Helvetica, Arial, san-serif',
   padding: 0
+});
+
+export const StyledTextField = styled(TextField)({
+  backgroundColor: '#FFFFFF'
 });
 
 export const StyledAccordionSummary = styled(AccordionSummary)({
