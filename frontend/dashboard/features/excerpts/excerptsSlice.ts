e108c2efc5096eb28cd5e7d5698b3b50f96addc0
Myles Moylan <moylanm@fastmail.com>
2024-03-08 13:46:39 -0800

    Add cases for updating state according to api requests

--- frontend/dashboard/features/excerpts/excerptsSlice.ts
+++ frontend/dashboard/features/excerpts/excerptsSlice.ts
@@ -1,4 +1,5 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter, createSlice, SerializedError } from '@reduxjs/toolkit';
+import { api } from '../api/apiSlice';
 import { RootState } from '../../app/store';
 import { Excerpt } from './types';
 
@@ -8,14 +9,47 @@ const excerptsAdapter = createEntityAdapter({
 
 const initialState = excerptsAdapter.getInitialState({
 	status: 'idle',
-	error: null
+	error: null as (SerializedError | null)
 });
 
 export const excerptsSlice = createSlice({
 	name: 'excerpts',
 	initialState,
-	reducers: {
-
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addMatcher(api.endpoints.getExcerpts.matchPending, (state) => {
+				state.status = 'loading';
+			})
+			.addMatcher(api.endpoints.getExcerpts.matchRejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.error;
+			})
+			.addMatcher(api.endpoints.getExcerpts.matchFulfilled, (state, { payload }) => {
+				state.status = 'succeeded';
+				state.ids = payload.map(excerpt => excerpt.id);
+				state.entities = payload.reduce((acc, excerpt) => acc[excerpt.id] = excerpt, {});
+			})
+			.addMatcher(api.endpoints.publishExcerpt.matchRejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.error;
+			})
+			.addMatcher(api.endpoints.publishExcerpt.matchFulfilled, (state, { payload }) => {
+				state.status = 'succeeded';
+				state.ids.push(payload.id);
+				state.entities[payload.id] = payload;
+			})
+			.addMatcher(api.endpoints.updateExcerpt.matchRejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.error;
+			})
+			.addMatcher(api.endpoints.updateExcerpt.matchFulfilled, (state, { payload }) => {
+				state.status = 'succeeded';
+				state.entities[payload.id] = payload;
+			})
+			.addMatcher(api.endpoints.deleteExcerpt.matchRejected, (state) => {
+				state.status = 'failed';
+			})
 	}
 });
 
