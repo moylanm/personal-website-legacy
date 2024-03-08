adff5ce7d508e11e0613f36d22ca0bbc5679f3ad
Myles Moylan <moylanm@fastmail.com>
2024-03-08 13:41:20 -0800

    Stub out Publisher component

---
+++ frontend/dashboard/features/excerpts/Publisher.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledTypography, StyledTextField } from './style';
+import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+
+const Publisher = () => {
+  
+};
+
+export default Publisher;
