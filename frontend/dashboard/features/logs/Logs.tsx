a8eddfedd5de3e2f03f03277221efc20fa6ad8f1
Myles Moylan <moylanm@fastmail.com>
2024-03-09 16:50:51 -0800

    #6 Set up Logs component to show FilterForm and RequestTable

--- frontend/dashboard/features/logs/Logs.tsx
+++ frontend/dashboard/features/logs/Logs.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useAppSelector } from '../../app/hooks';
+import { selectAllLogs } from './logsSlice';
+import FilterForm from './FilterForm';
+import LogTable from './LogTable';
 
 
 const Logs = () => {
+	const requestLogs = useAppSelector(selectAllLogs);
+	const ipAddresses = useAppSelector(state => state.logs.ipAddresses);
+
+	const filteredRequests = useMemo(() => {
+		const selectedIps = ipAddresses
+			.filter((ip) => ip.selected)
+			.map((ip) => ip.value);
+
+		return requestLogs.filter((request) => selectedIps.includes(request.ipAddress));
+	}, [requestLogs, ipAddresses]);
+
+	return (
+		<>
+			<FilterForm />
+			{filteredRequests.length > 0
+				? <LogTable requests={filteredRequests} />
+				: <div className='message'>No logs to render...</div>}
+		</>
+	);
 };
 
 export default Logs;
