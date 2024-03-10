7194c1074d4b9903c8c5524e05a91d7c94c398e2
Myles Moylan <moylanm@fastmail.com>
2024-03-10 12:22:18 -0700

    Add style for snackbar

---
+++ frontend/dashboard/components/style.ts
@@ -0,0 +1,7 @@
+import { styled } from '@mui/system';
+import Typography from '@mui/material/Typography';
+
+export const StyledTypography = styled(Typography)({
+  fontStyle: 'Roboto, Helvetica, Arial, san-serif',
+  padding: 0
+});
