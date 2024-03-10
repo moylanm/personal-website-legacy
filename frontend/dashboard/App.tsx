cc15b9f08f7d25fbd3e7330cb02f6d40d6b5dac7
Myles Moylan <moylanm@fastmail.com>
2024-03-10 12:23:14 -0700

    Add support for snackbar responses

--- frontend/dashboard/App.tsx
+++ frontend/dashboard/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useGetExcerptsQuery, useGetLogsQuery } from './features/api/apiSlice';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { resetStatus } from './features/excerpts/excerptsSlice';
 import TabBar from './features/tabs/TabBar';
+import ResponseSnackbar from './components/Snackbar';
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  const status = useAppSelector(state => state.excerpts.status);
+  const error = useAppSelector(state => state.excerpts.error);
+  const statusMessage = useAppSelector(state => state.excerpts.statusMessage);
+
   const {
     isLoading: excerptsLoading,
     isSuccess: excerptsSuccess,
@@ -20,12 +28,21 @@ const App = () => {
   const isLoading = excerptsLoading || logsLoading;
   const isSuccess = excerptsSuccess && logsSuccess;
 
+  const handleSnackbarClose = useCallback(() => {
+    dispatch(resetStatus());
+  }, [dispatch, resetStatus]);
+
   return (
     <>
       {isLoading && <div className='message'>Loading...</div>}
       {excerptsError && <div className='error-message'>{excerptsErrorMessage.toString()}</div>}
       {logsError && <div className='error-message'>{logsErrorMessage.toString()}</div>}
       {isSuccess && <TabBar />}
+
+      {status === 'succeeded' &&
+        <ResponseSnackbar severity='success' response={statusMessage} handleClose={handleSnackbarClose} />}
+      {status === 'failed' &&
+        <ResponseSnackbar severity='error' response={error?.message!} handleClose={handleSnackbarClose} />}
     </>
   );
 };
