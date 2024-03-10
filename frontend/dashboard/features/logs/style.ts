60a71a4db1a281d74070b78fea30f66b429d5f8f
Myles Moylan <moylanm@fastmail.com>
2024-03-09 16:48:45 -0800

    Create style file

---
+++ frontend/dashboard/features/logs/style.ts
@@ -0,0 +1,7 @@
+import { styled } from '@mui/system';
+import TableContainer from '@mui/material/TableContainer';
+
+export const StyledTableContainer = styled(TableContainer)({
+  width: 'initial',
+  margin: '2px calc(-1 * ((100% - 550px) / 2)) 0'
+});
