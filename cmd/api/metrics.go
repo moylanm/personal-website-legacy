c64af571579e462ff9047bd35c84b35fa946d2f6
Myles Moylan <moylanm@fastmail.com>
2024-03-13 10:55:40 -0700

    Add uptime metric

--- cmd/api/metrics.go
+++ cmd/api/metrics.go
@@ -4,9 +4,17 @@ import (
 	"database/sql"
 	"expvar"
 	"runtime"
+	"time"
 )
 
+var startTime time.Time
+
 func publishMetrics(db *sql.DB) {
+	expvar.Publish("uptime", expvar.Func(func() any {
+		uptime := time.Since(startTime)
+		return uptime.Round(time.Second).String()
+	}))
+
 	expvar.Publish("goroutines", expvar.Func(func() any {
 		return runtime.NumGoroutine()
 	}))
