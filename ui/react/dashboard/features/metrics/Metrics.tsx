430aa0cac31b6e6f13d013cce7835f5db1814a54
Myles Moylan <moylanm@fastmail.com>
2024-10-20 08:50:38 -0700

    Fix import

--- ui/react/dashboard/features/metrics/Metrics.tsx
+++ ui/react/dashboard/features/metrics/Metrics.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import { useCallback } from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { useGetMetricsQuery } from '../api/apiSlice';
 import Button from '@mui/material/Button';
