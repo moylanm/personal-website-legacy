1d8551545bf928c03e8930086dd522027c9f2edd
Myles Moylan <moylanm@fastmail.com>
2024-10-20 08:00:42 -0700

    Fall back to document body in case `excerpts-root` not found

--- ui/react/excerpts/excerpts.tsx
+++ ui/react/excerpts/excerpts.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('excerpts-root')!);
+const root = createRoot(document.getElementById('excerpts-root') ?? document.body);
 root.render(
   <React.StrictMode>
     <App />
